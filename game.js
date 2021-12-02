0dca0c713201f2e2f3ed1d2fc34e265b69cb7b34
Sparks989 <89668312+Sparks989@users.noreply.github.com>
2021-12-02 16:47:09 -0500

    Timer?

--- game.js
+++ game.js
@@ -248,7 +248,7 @@ scene("game", ({ levelId, score } = {levelId: 0, score: 0}) => {
     victor.play();
   }
 
-  playAudio();
+  music.play();
   
   document.getElementById("music").volume = 0.5;
   document.getElementById("damage").volume = 0.8;
